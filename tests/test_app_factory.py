fcf34fddbf7b1ad93a136fb6b5319ba0c7b0a388
igortxra <igor.teixeira@cromai.com>
2023-11-04 13:41:18 -0300

    style: apply lint suggestions

--- tests/test_app_factory.py
+++ tests/test_app_factory.py
@@ -5,26 +5,27 @@ from flask.testing import FlaskClient
 from app import create_app
 
 
-@pytest.fixture
-def client():
+@pytest.fixture(name="client")
+def create_test_client():
+    """Returns FlaskClient instance for test purposes."""
     app = create_app()
     app.testing = True
     return app.test_client()
 
 
 def test_app_factory_returns_app():
-    description = "app factory must return an Flask instance"
+    """App factory must return an Flask instance."""
     app = create_app()
-    assert isinstance(app, Flask), description
+    assert isinstance(app, Flask)
 
 
 def test_app_index_route_return(client: FlaskClient):
-    description = "app index route must return status 200"
+    """App index route must return status 200"""
     response = client.get("/")
-    assert response.status_code == 200, description
+    assert response.status_code == 200
 
 
 def test_app_index_body_return(client: FlaskClient):
-    description = "app index route must return empty json"
+    """App index route must return empty json"""
     response = client.get("/")
-    assert response.get_json() == {}, description
+    assert response.get_json() == {}
